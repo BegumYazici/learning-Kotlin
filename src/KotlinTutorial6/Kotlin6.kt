01335b1c4b68c755984a912de58581e53cbad8e1
Begum.Yazici <yazicibegum@gmail.com>
2018-01-08 23:43:15 +0300

    Commited

---
+++ src/KotlinTutorial6/Kotlin6.kt
@@ -0,0 +1,12 @@
+package KotlinTutorial6
+
+fun main(args: Array<String>) {
+
+    var str1: String = "deneme"
+    var str2: String = "Deneme"
+
+    if (str1.equals(str2))
+        println("Same")
+    else
+        println("Not same")
+}
